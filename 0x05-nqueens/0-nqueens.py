75a746261479433ea27901cf76b66a863630e80b
KevGuama <kevguama@gmail.com>
2024-08-29 23:18:16 +0300

    0x05. N Queens_3.0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,64 +1,45 @@
 #!/usr/bin/python3
-"""
-Solution to the N queens problem
-"""
+
+""" N queens """
 import sys
 
-def backtrack(row, n, cols, pos_diag, neg_diag, board):
-    """
-    Backtrack function to find all solutions
-    """
-    if row == n:
-        res = []
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 1:
-                    res.append([i, j])
-        print(res)
-        return
 
-    for col in range(n):
-        if col in cols or (row + col) in pos_diag or (row - col) in neg_diag:
-            continue
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-        # Place the queen
-        cols.add(col)
-        pos_diag.add(row + col)
-        neg_diag.add(row - col)
-        board[row][col] = 1
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-        # Move to the next row
-        backtrack(row + 1, n, cols, pos_diag, neg_diag, board)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-        # Remove the queen (backtrack)
-        cols.remove(col)
-        pos_diag.remove(row + col)
-        neg_diag.remove(row - col)
-        board[row][col] = 0
+n = int(sys.argv[1])
 
-def nqueens(n):
-    """
-    Solves the N queens problem.
-    Args:
-        n (int): number of queens. Must be >= 4
-    """
-    cols = set()
-    pos_diag = set()
-    neg_diag = set()
-    board = [[0] * n for _ in range(n)]
-    backtrack(0, n, cols, pos_diag, neg_diag, board)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    try:
-        N = int(sys.argv[1])
-        if N < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
