f7406eee405ea84e513b9071816a3f45ad24cd1e
KevGuama <kevguama@gmail.com>
2024-09-05 23:44:43 +0300

    0x06. Star Wars API_2.2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,67 @@
 
 const request = require('request');
 
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
+// Define the movie API endpoint
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+// Function to fetch movie details and get the character URLs
+const getCharacters = async () => {
+  return new Promise((resolve, reject) => {
+    request(filmEndPoint, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        reject(new Error(`Error: ${err} | StatusCode: ${res ? res.statusCode : 'unknown'}`));
+      } else {
+        const jsonBody = JSON.parse(body);
+        resolve(jsonBody.characters);
+      }
+    });
+  });
 };
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
+// Function to fetch character names from a list of character URLs
+const getCharacterNames = async (characters) => {
+  // Use Promise.all to fetch all character names in parallel
+  const promises = characters.map((url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
         if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+          reject(new Error(`Error: ${err} | StatusCode: ${res ? res.statusCode : 'unknown'}`));
         } else {
           const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
+          resolve(jsonBody.name);
         }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
+      });
+    });
+  });
+
+  // Wait for all character requests to complete and return the names
+  return Promise.all(promises);
+};
+
+// Main function to coordinate the fetching and printing of characters
+const printCharacterNames = async () => {
+  try {
+    // Step 1: Get the characters for the movie
+    const characters = await getCharacters();
+
+    // Step 2: Fetch all character names in parallel
+    const names = await getCharacterNames(characters);
+
+    // Step 3: Print each character's name in order
+    names.forEach((name, index) => {
+      if (index === names.length - 1) {
+        process.stdout.write(name);
+      } else {
+        process.stdout.write(name + '\n');
+      }
+    });
+  } catch (error) {
+    console.error(error.message);
   }
 };
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+// Execute the main function
+printCharacterNames();
