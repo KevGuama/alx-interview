520f811d437a840dcf2b6c01a55aaefb4dd72650
KevGuama <kevguama@gmail.com>
2024-09-06 22:20:40 +0300

    0x03. Log Parsing_debug_exe2.1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""
-validUTF8
-"""
-
-
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
-    count = 0
+    # Number of bytes remaining in the current UTF-8 character
+    bytes_remaining = 0
 
-    for x in data:
+    for num in data:
+        # Check if num is a valid byte (between 0 and 255)
+        if num > 255:
+            return False
 
-        if 191 >= x >= 128:
-
-            if not count:
-                return False
-
-            count -= 1
-        else:
-            if count:
-                return False
-
-            if x < 128:
+        # If no bytes are expected, determine the number of bytes in the current character
+        if bytes_remaining == 0:
+            # 1-byte character
+            if num >> 7 == 0:
                 continue
-            elif x < 224:
-                count = 1
-            elif x < 240:
-                count = 2
-            elif x < 248:
-                count = 3
+            # 2-byte character (starts with 110xxxxx)
+            elif num >> 5 == 0b110:
+                bytes_remaining = 1
+            # 3-byte character (starts with 1110xxxx)
+            elif num >> 4 == 0b1110:
+                bytes_remaining = 2
+            # 4-byte character (starts with 11110xxx)
+            elif num >> 3 == 0b11110:
+                bytes_remaining = 3
             else:
                 return False
+        else:
+            # Check if the current byte is a continuation byte (starts with 10xxxxxx)
+            if num >> 6 != 0b10:
+                return False
+            bytes_remaining -= 1
 
-    return count == 0
+    # If we've consumed all expected continuation bytes, it's valid
+    return bytes_remaining == 0
